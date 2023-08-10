826937e528ca1b8a6031eb48a50edbc72e005dd1
Kenytt Avery <kavery@fullerton.edu>
2023-08-09 22:55:56 -0700

    Example of rounding vs. type coercion

---
+++ rounding.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    cout << "int(1.5) = " << int(1.5) << endl;
+    cout << "round(1.5) = " << round(1.5) << endl;
+}
