5bcd4599020d67716a175572ce61517ff6dcd453
Kenytt Avery <kavery@fullerton.edu>
2023-08-09 20:25:46 -0700

    Examples of integer division and modulus

---
+++ integer-division.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "14 / 4 = " << 14 / 4 << endl;
+    cout << "5 / 13 = " << 5 / 13 << endl;
+    cout << "7 / 2.0 = " << 7 / 2.0 << endl;
+
+    cout << "14 % 4 = " << 14 % 4 << endl;
+    cout << "5 % 13 = " << 5 % 13 << endl;
+    // cout << "7 % 2.0 = " << 7 % 2.0 << endl;
+}
