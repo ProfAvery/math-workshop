8d28009d2d042c9d42fa765b433b2b7be1e0b6e5
Kenytt Avery <kavery@fullerton.edu>
2023-08-09 22:26:12 -0700

    Example of floating-point weirdness

---
+++ floating-point.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << boolalpha;
+
+    cout << "1 + 2 = " << 1 + 2 << endl;
+    cout << "3 = " << 3 << endl;
+
+    cout << "(1 + 2 == 3)? " << (1 + 2 == 3) << endl;
+    cout << endl;
+
+
+    cout << "1.0 + 2.0 = " << 1.0 + 2.0 << endl;
+    cout << "3.0 = " << 3.0 << endl;
+
+    cout << "(1.0 + 2.0 == 3.0)? " << (1.0 + 2.0 == 3.0) << endl;
+    cout << endl;
+
+
+    cout << "0.1 + 0.2 = " << 0.1 + 0.2 << endl;
+    cout << "0.3 = " << 0.3 << endl;
+
+    cout << "(0.1 + 0.2 == 0.3)? " << (0.1 + 0.2 == 0.3) << endl;
+    cout << endl;
+
+
+    cout << "0.125 + 0.25 = " << 0.125 + 0.25 << endl;
+    cout << "0.375 = " << 0.375 << endl;
+
+    cout << "(0.125 + 0.25 == 0.375)? " << (0.125 + 0.25 == 0.375) << endl;
+    cout << endl;
+}
